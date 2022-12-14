7ec0d1c4490faa22471c44a9aa77e28463c45c8a
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2022-12-14 13:44:13 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -427,7 +427,7 @@ def register_complete():
     ip=request.remote_addr
     ip1=encuname[1]
     if not ip==ip1:
-	abort(401)
+        abort(401)
     credentials=read_key(uname)
     data = cbor.decode(request.get_data())
     client_data = ClientData(data["clientDataJSON"])
